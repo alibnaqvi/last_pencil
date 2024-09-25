1f6cf463ac0cc672219877ff910d64e0f895c936
Ali Naqvi <alibnaqvi851@gmail.com>
2024-09-24 21:07:05 -0700

    Task 2 complete

--- Last Pencil (Java)/task/src/Main.java
+++ Last Pencil (Java)/task/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many pencils would you like to use: ");
+        int pencils = scanner.nextInt();
+
+        System.out.print("Who will be the first (John, Jack): ");
+        String name = scanner.next();
+
+        System.out.println("|".repeat(pencils));
+
+        System.out.println(name + " is going first!");
     }
 }
